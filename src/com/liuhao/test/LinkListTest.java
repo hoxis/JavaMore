5435104539ae399004a18a5e9cbf192abb3de150
liuhao <liuhao1960@gmail.com>
2014-04-13 20:08:48 +0800

    单向链表的实现：新增测试部分，删除元素，删除指定位置的元素

--- src/com/liuhao/test/LinkListTest.java
+++ src/com/liuhao/test/LinkListTest.java
@@ -8,42 +8,56 @@ public class LinkListTest {
 
 	@Test
 	public void test() {
-		
-		//测试构造函数
+
+		// 测试构造函数
 		LinkList<String> list = new LinkList("好");
 		System.out.println(list);
-		
-		//测试添加元素
+
+		// 测试添加元素
 		list.add("ni");
 		list.add("没");
 		System.out.println(list);
-		
-		//在头部添加
+
+		// 在头部添加
 		list.addAtHead("五月");
 		System.out.println(list);
-		
-		//在指定位置添加
+
+		// 在指定位置添加
 		list.insert("摩卡", 2);
 		System.out.println(list);
-		
-		//获取指定位置处的元素
+
+		// 获取指定位置处的元素
 		System.out.println("第2个元素是（从0开始计数）：" + list.get(2));
-		
-		//返回元素索引
+
+		// 返回元素索引
 		System.out.println("摩卡在的位置是：" + list.locate("摩卡"));
 		System.out.println("moka所在的位置：" + list.locate("moka"));
-		
-		//获取长度
+
+		// 获取长度
 		System.out.println("当前线性表的长度：" + list.length());
-		
-		//判断是否为空
+
+		// 判断是否为空
 		System.out.println(list.isEmpty());
-		
-		//清空
+
+		// 删除最后一个元素
+		list.remove();
+		System.out.println("调用remove()后：" + list);
+
+		// 获取长度
+		System.out.println("当前线性表的长度：" + list.length());
+
+		// 删除指定位置处元素
+		list.delete(3);
+		System.out.println("删除第4个元素后：" + list);
+
+		// 获取长度
+		System.out.println("当前线性表的长度：" + list.length());
+
+		// 清空
 		list.clear();
 		System.out.println(list);
-		
-		//判断是否为空
+
+		// 判断是否为空
 		System.out.println(list.isEmpty());
 	}
 
