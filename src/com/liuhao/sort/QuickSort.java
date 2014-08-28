4d4be91ae15ecfc16c974c148c62b26c1b5d101e
hoxis <liuhao1960@gmail>
2014-08-28 21:38:52 +0800

    添加一些程序注释

--- src/com/liuhao/sort/QuickSort.java
+++ src/com/liuhao/sort/QuickSort.java
@@ -10,22 +10,30 @@ import java.util.Arrays;
 public class QuickSort {
 
 	/**
+	 * 对序列data，从start到end进行一趟处理
 	 * @param data
 	 * @param start
 	 * @param end
 	 */
 	private static int subSort(DataWrap[] data, int start, int end){
 
+		//将start处的元素值作为基准
 		DataWrap tmp = data[start];
+		
 		while(start < end){
+			
+			//从尾部开始找小于基准的元素索引，直到找到满足这样条件的索引
 			while( start < end && data[end].compareTo(tmp) >= 0){
 				end--;
 			}
+			//用找到的小于基准的元素值覆盖start处的元素值，start处的元素值已经备份在tmp中
 			data[start] = data[end];
 			
+			//从首部开始找大于基准的元素索引
 			while(start < end && data[start].compareTo(tmp) <= 0){
 				start++;
 			}
+			//用找到的大于基准的元素值覆盖end处的元素值，end处的元素值已经覆盖在之前的start处
 			data[end] = data[start];
 		}
 		
